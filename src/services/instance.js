4838beeff6eaaef544970ea876515af5eb2bbecc
Manojprabhagar90 <manojprabhagar90@gmail.com>
2025-04-18 17:37:42 +0530

    Update instance.js

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 // define the base url
-const baseURL = 'http://localhost:3002/api/v1';
-
+//const baseURL = 'http://localhost:3002/api/v1';
+const baseURL = 'https://password-reset-h1ej.onrender.com/api/v1';
 // create an axios instance
 const instance = axios.create({
     baseURL,
